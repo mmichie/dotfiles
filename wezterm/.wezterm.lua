ec95fdb30f1fe722f261e8bc734551f8b75713f0
Matt Michie <mmichie@gmail.com>
2024-11-27 22:20:20 -0800

    feat(wezterm): enhance configuration and add keybindings
    - Refactor configuration variable from 'config' to 'c'
    - Add key bindings for switching tabs with CMD+Arrow keys
    - Implement tab bar hiding when only one tab is open
    - Adjust font rendering settings to mimic iTerm2
    - Disable window size adjustment when changing font size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,16 +2,37 @@
 local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
-local config = wezterm.config_builder()
+local c = wezterm.config_builder()
 
-config.font = wezterm.font("Inconsolata for Powerline", {weight="Medium", stretch="Normal", style="Normal"})
-config.font_size = 18
+c.font = wezterm.font("Inconsolata for Powerline", {weight="Medium", stretch="Normal", style="Normal"})
+c.font_size = 18
+
 -- config.color_scheme = '3024 Night'
 -- config.color_scheme = 'Deep'
 -- config.color_scheme = 'CGA'
-config.color_scheme = 'Firefly Traditional'
--- config.color_scheme = 'Dark Ocean'
-config.freetype_load_target = "HorizontalLcd"
 -- config.freetype_load_target = "Light"
+-- config.color_scheme = 'Dark Ocean'
+c.color_scheme = 'Firefly Traditional'
+c.hide_tab_bar_if_only_one_tab = true
 
-return config
+-- Add key bindings for switching tabs
+c.keys = {
+  {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action = wezterm.action.ActivateTabRelative(1),
+  },
+}
+
+-- Font rendering settings to be more like iTerm2
+c.freetype_load_target = "Light"
+c.freetype_render_target = "HorizontalLcd"
+c.font_rasterizer = "FreeType"  -- Using FreeType for rendering
+c.adjust_window_size_when_changing_font_size = false
+
+return c
