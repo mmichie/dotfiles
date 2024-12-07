f4d3e946cd3fbd7f1bf65249a7102a5aa9f9b90e
Matt Michie <mmichie@gmail.com>
2024-12-07 15:04:55 -0800

    feat: enhance neovim configuration
    - Add bufferline for improved tab management.
    - Add toggleterm for integrated terminal.
    - Add go.nvim for Go development support.
    - Add nvim-dap and dependencies for debugging.
    - Add symbols-outline for code outline view.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -37,13 +37,34 @@ return {
         end,
     },
 
+    -- Enhanced Tab Management
+    {
+        "akinsho/bufferline.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("bufferline").setup({
+                options = {
+                    mode = "tabs",
+                    separator_style = "slant",
+                    always_show_bufferline = true,
+                    show_buffer_close_icons = true,
+                    show_close_icon = true,
+                    color_icons = true
+                }
+            })
+            -- Keymaps for tab navigation
+            vim.keymap.set('n', '<S-l>', ':BufferLineCycleNext<CR>')
+            vim.keymap.set('n', '<S-h>', ':BufferLineCyclePrev<CR>')
+        end
+    },
+
     -- Syntax highlighting
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         config = function()
             require('nvim-treesitter.configs').setup({
-                ensure_installed = { "lua", "vim", "python", "javascript", "markdown" },
+                ensure_installed = { "lua", "vim", "python", "javascript", "markdown", "go" },
                 highlight = { enable = true },
                 indent = { enable = true },
             })
@@ -71,7 +92,6 @@ return {
                 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
                 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                -- Use format instead of formatting
                 vim.keymap.set('n', '<leader>f', function()
                     vim.lsp.buf.format({ async = true })
                 end, opts)
@@ -79,8 +99,11 @@ return {
 
             -- Configure Python LSP
             require('lspconfig').pyright.setup({})
+            -- Configure Go LSP
+            require('lspconfig').gopls.setup({})
         end
     },
+
     -- File explorer
     {
         "nvim-neo-tree/neo-tree.nvim",
@@ -109,6 +132,20 @@ return {
         end
     },
 
+    -- Terminal Integration
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup({
+                size = 20,
+                open_mapping = [[<c-\>]],
+                hide_numbers = true,
+                shade_terminals = true,
+                direction = "float",
+            })
+        end
+    },
+
     -- Git integration
     {
         'lewis6991/gitsigns.nvim',
@@ -117,45 +154,116 @@ return {
         end
     },
 
-   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-        require("copilot").setup({
-            panel = {
-                enabled = true,
-                auto_refresh = true,
-                keymap = {
-                    jump_prev = "[[",
-                    jump_next = "]]",
-                    accept = "<CR>",
-                    refresh = "gr",
-                    open = "<M-CR>"
+    -- Go Development
+    {
+        "ray-x/go.nvim",
+        dependencies = {
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("go").setup({
+                -- Go configuration
+                go = 'go', -- Go binary path
+                goimport = 'gopls', -- Import organizer
+                fillstruct = 'gopls',
+                gofmt = 'gofumpt',
+                max_line_len = 120,
+                tag_transform = false,
+                test_template = '', -- default to testify if not set
+                test_template_dir = '',
+                comment_placeholder = '',
+                icons = { breakpoint = '🧘', currentpos = '🏃' },
+                verbose = false,
+                lsp_cfg = true, -- false: use your own lspconfig
+                lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
+                lsp_on_attach = true, -- use on_attach from go.nvim
+                dap_debug = true,
+            })
+        end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()'
+    },
+
+    -- Debugging support
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            "mfussenegger/nvim-dap-python",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function()
+            local dap = require('dap')
+            local dapui = require('dapui')
+
+            -- Python debugger setup
+            require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+
+            -- Debugger UI
+            dapui.setup()
+
+            -- Debugger keymaps
+            vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint)
+            vim.keymap.set('n', '<leader>dc', dap.continue)
+            vim.keymap.set('n', '<leader>ds', dap.step_over)
+            vim.keymap.set('n', '<leader>di', dap.step_into)
+            vim.keymap.set('n', '<leader>do', dap.step_out)
+            vim.keymap.set('n', '<leader>du', dapui.toggle)
+        end
+    },
+
+    -- Code outline/symbols
+    {
+        'simrat39/symbols-outline.nvim',
+        config = function()
+            require('symbols-outline').setup()
+            vim.keymap.set('n', '<leader>so', ':SymbolsOutline<CR>')
+        end
+    },
+
+    -- Copilot
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                panel = {
+                    enabled = true,
+                    auto_refresh = true,
+                    keymap = {
+                        jump_prev = "[[",
+                        jump_next = "]]",
+                        accept = "<CR>",
+                        refresh = "gr",
+                        open = "<M-CR>"
+                    },
                 },
-            },
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                debounce = 75,
-                keymap = {
-                    accept = "<Tab>",
-                    accept_word = "<M-w>",
-                    accept_line = "<M-l>",
-                    next = "<M-]>",
-                    prev = "<M-[>",
-                    dismiss = "<C-]>",
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                    debounce = 75,
+                    keymap = {
+                        accept = "<Tab>",
+                        accept_word = "<M-w>",
+                        accept_line = "<M-l>",
+                        next = "<M-]>",
+                        prev = "<M-[>",
+                        dismiss = "<C-]>",
+                    },
                 },
-            },
-            filetypes = {
-                yaml = true,
-                markdown = true,
-                help = false,
-                gitcommit = false,
-                gitrebase = false,
-                ["."] = false,
-            },
-        })
-    end,
-  }
+                filetypes = {
+                    yaml = true,
+                    markdown = true,
+                    help = false,
+                    gitcommit = false,
+                    gitrebase = false,
+                    ["."] = false,
+                },
+            })
+        end,
+    }
 }
