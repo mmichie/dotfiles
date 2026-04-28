a49c26ee56beb9fe30b3742eedd42389abb94d34
Matt Michie <mmichie@gmail.com>
2026-04-28 13:55:46 -0700

    feat(tensor9): manage loopback sudo rule via nix-darwin
    Adds the NOPASSWD ifconfig lo0 rule used for tensor9 local testing as
    security.sudo.extraConfig on the tensor9-mbp host so it survives a
    fresh install. Replaces the manual /etc/sudoers.d/t9-test-loopback
    file, which was also setting Defaults:mim secure_path globally and
    broke sudo PATH resolution for nix tools.

--- hosts/tensor9-mbp/configuration.nix
+++ hosts/tensor9-mbp/configuration.nix
@@ -1,4 +1,9 @@
 {
   imports = [ ../../modules/darwin/workstation-base.nix ];
   networking.hostName = "tensor9-mbp";
+
+  # Loopback alias management for tensor9 local testing.
+  security.sudo.extraConfig = ''
+    mim ALL=(ALL) NOPASSWD: /sbin/ifconfig lo0 alias *, /sbin/ifconfig lo0 inet * delete
+  '';
 }
