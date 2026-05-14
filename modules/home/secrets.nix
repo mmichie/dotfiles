4ac3061ff234db10788637ab2ecf60871717195b
Matt Michie <mmichie@gmail.com>
2026-05-13 17:12:40 -0700

    feat(secrets): manage .zshrc.local, .env, .cargo/credentials.toml via sops-nix

--- modules/home/secrets.nix
+++ modules/home/secrets.nix
@@ -9,11 +9,34 @@
     defaultSopsFile = ../../secrets/atuin-key;
     validateSopsFiles = true;
 
-    secrets."atuin_key" = {
-      format = "binary";
-      sopsFile = ../../secrets/atuin-key;
-      path = "${config.home.homeDirectory}/.local/share/atuin/key";
-      mode = "0400";
+    secrets = {
+      "atuin_key" = {
+        format = "binary";
+        sopsFile = ../../secrets/atuin-key;
+        path = "${config.home.homeDirectory}/.local/share/atuin/key";
+        mode = "0400";
+      };
+
+      "zshrc_local" = {
+        format = "binary";
+        sopsFile = ../../secrets/zshrc-local;
+        path = "${config.home.homeDirectory}/.zshrc.local";
+        mode = "0400";
+      };
+
+      "dotenv" = {
+        format = "binary";
+        sopsFile = ../../secrets/env;
+        path = "${config.home.homeDirectory}/.env";
+        mode = "0400";
+      };
+
+      "cargo_credentials" = {
+        format = "binary";
+        sopsFile = ../../secrets/cargo-credentials;
+        path = "${config.home.homeDirectory}/.cargo/credentials.toml";
+        mode = "0400";
+      };
     };
   };
 }
