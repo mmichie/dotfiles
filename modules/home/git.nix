8004e6cc47c4473b6a46056e63e18595f6581a78
Matt Michie <mmichie@gmail.com>
2026-05-21 14:26:29 -0700

    fix(git): gate kyusu-local include behind my.isWork and clear statix warning
    Personal hosts (mims-mbp, tensor9-mbp) were getting .gitconfig-kyusu-local
    symlinked into $HOME even though its includeIf only fires under ~/src/moab/.
    Wrap the symlink in lib.optionalAttrs config.my.isWork so only work hosts
    receive it, and collapse the three sibling home.file."X".source assignments
    into one attrset so statix stops failing the pre-commit hook on any change
    to this file.

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,6 +1,15 @@
-{ mkLink, ... }:
 {
-  home.file.".gitconfig".source = mkLink "git/.gitconfig";
-  home.file.".gitignore_global".source = mkLink "git/.gitignore_global";
-  home.file.".gitconfig-kyusu-local".source = mkLink "git/.gitconfig-kyusu-local";
+  lib,
+  config,
+  mkLink,
+  ...
+}:
+{
+  home.file = {
+    ".gitconfig".source = mkLink "git/.gitconfig";
+    ".gitignore_global".source = mkLink "git/.gitignore_global";
+  }
+  // lib.optionalAttrs config.my.isWork {
+    ".gitconfig-kyusu-local".source = mkLink "git/.gitconfig-kyusu-local";
+  };
 }
